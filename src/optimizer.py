e69f87ebb7176a791f41619e65063cd483e0b4a3
Ali Wasif <141522236+syncali@users.noreply.github.com>
2025-12-07 11:50:41 +0500

    Update Optimizer

--- src/optimizer.py
+++ src/optimizer.py
@@ -18,29 +18,13 @@ def to_int(value):
 
 
 def constant_fold(code):
-    env = {}
     new_code = []
     for quad in code:
         op, a1, a2, res = quad.op, quad.arg1, quad.arg2, quad.result
-        if op == "=":
-            if is_int_literal(a1):
-                env[res] = to_int(a1)
-            else:
-                env.pop(res, None)
-            new_code.append(quad)
-            continue
         if op in {"+", "-", "*", "/"}:
-            v1 = None
-            v2 = None
-            if is_int_literal(a1):
+            if is_int_literal(a1) and is_int_literal(a2):
                 v1 = to_int(a1)
-            elif a1 in env:
-                v1 = env[a1]
-            if is_int_literal(a2):
                 v2 = to_int(a2)
-            elif a2 in env:
-                v2 = env[a2]
-            if v1 is not None and v2 is not None:
                 if op == "+":
                     val = v1 + v2
                 elif op == "-":
@@ -51,37 +35,24 @@ def constant_fold(code):
                     if v2 == 0:
                         raise CompilerError(0, "Division by zero")
                     val = v1 // v2
-                env[res] = val
                 new_code.append(type(quad)("=", str(val), None, res))
             else:
-                env.pop(res, None)
                 new_code.append(quad)
             continue
         if op in {">", "<", "=="}:
-            v1 = None
-            v2 = None
-            if is_int_literal(a1):
+            if is_int_literal(a1) and is_int_literal(a2):
                 v1 = to_int(a1)
-            elif a1 in env:
-                v1 = env[a1]
-            if is_int_literal(a2):
                 v2 = to_int(a2)
-            elif a2 in env:
-                v2 = env[a2]
-            if v1 is not None and v2 is not None:
                 if op == ">":
                     val = 1 if v1 > v2 else 0
                 elif op == "<":
                     val = 1 if v1 < v2 else 0
                 else:
                     val = 1 if v1 == v2 else 0
-                env[res] = val
                 new_code.append(type(quad)("=", str(val), None, res))
             else:
-                env.pop(res, None)
                 new_code.append(quad)
             continue
-        env.pop(res, None)
         new_code.append(quad)
     return new_code
 
